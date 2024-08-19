3b997b01e12e4d4b67534e764c50ce975928babc
AlisonAndrade123 <andradealison291@gmail.com>
2024-08-19 10:55:03 -0300

    Lista 5

--- L5Q5R.py
+++ L5Q5R.py
@@ -1,10 +1,10 @@
-def fibonacci(n):
-    if n <= 2:
+def fatorial(n):
+    if n == 0 or n == 1:
         return 1
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2) 
+        return n * fatorial(n - 1) 
 
 num = int(input())
 while num != -1:
-    print(fibonacci(num))
+    print(fatorial(num))
     num = int(input())
